48c22b25743eb1f4d8d214a22f3744f9b43f42cd
Fernando Leme <fermleme@hotmail.com>
2020-07-25 17:04:24 -0300

    Criado Tela de listagem de Eventos

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from core.models import Evento
+
 # Register your models here.
 class EventoAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'data_evento', 'data_criacao')
